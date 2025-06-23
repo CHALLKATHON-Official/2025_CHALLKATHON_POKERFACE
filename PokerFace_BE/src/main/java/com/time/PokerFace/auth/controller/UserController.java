a0cf0390b2500a759f1a1398bd2d7c28d49d1828
forwoni <ktion0619@naver.com>
2025-06-23 12:37:02 +0900

    feat: add UserController

---
+++ PokerFace_BE/src/main/java/com/time/PokerFace/auth/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.time.PokerFace.auth.controller;
+
+import com.time.PokerFace.auth.dto.EmailChangeRequest;
+import com.time.PokerFace.auth.dto.PasswordChangeRequest;
+import com.time.PokerFace.auth.dto.ProfileUpdateRequest;
+import com.time.PokerFace.auth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getMyProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userService.getMyInfo(userDetails.getUsername()));
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<?> changePassword(@AuthenticationPrincipal UserDetails userDetails,
+                                            @RequestBody PasswordChangeRequest dto) {
+        userService.updatePassword(userDetails.getUsername(), dto);
+        return ResponseEntity.ok("비밀번호 변경 완료");
+    }
+
+    @PatchMapping("/email")
+    public ResponseEntity<?> changeEmail(@AuthenticationPrincipal UserDetails userDetails,
+                                         @RequestBody EmailChangeRequest dto) {
+        userService.updateEmail(userDetails.getUsername(), dto);
+        return ResponseEntity.ok("이메일 변경 완료");
+    }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<?> updateProfile(@AuthenticationPrincipal UserDetails userDetails,
+                                           @RequestBody ProfileUpdateRequest dto) {
+        userService.updateProfile(userDetails.getUsername(), dto);
+        return ResponseEntity.ok("프로필 업데이트 완료");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUser(@AuthenticationPrincipal UserDetails userDetails) {
+        userService.deleteUser(userDetails.getUsername());
+        return ResponseEntity.ok("회원 탈퇴 완료");
+    }
+}
